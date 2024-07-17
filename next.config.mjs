a3a9736894b4d66c7400091f198d5cc8163af6fc
Olanrewaju <lawallanre.abbahouse@gmail.com>
2024-07-17 08:12:41 +0100

    test

--- next.config.mjs
+++ next.config.mjs
@@ -11,7 +11,11 @@ const nextConfig = {
     experimental: {
       taint: true,
     },
-    // ...other config settings
+    eslint: {
+      // Warning: This allows production builds to successfully complete even if
+      // your project has ESLint errors.
+      ignoreDuringBuilds: true,
+    },
   };
   
   export default nextConfig;
