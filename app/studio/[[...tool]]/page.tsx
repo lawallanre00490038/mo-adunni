fdc36e475d003624b95a0500e52abd2129d5345e
Olanrewaju <lawallanre.abbahouse@gmail.com>
2024-07-17 08:25:38 +0100

    test

--- app/studio/[[...tool]]/page.tsx
+++ app/studio/[[...tool]]/page.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 /**
  * This route is responsible for the built-in authoring environment using Sanity Studio.
  * All routes under your studio path is handled by this file using Next.js' catch-all routes:
